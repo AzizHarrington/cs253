21fabf5e21de89102739a78ccfde5759f912d7c8
azizharrington <azizharrington@gmail.com>
2015-02-02 08:37:28 -0600

    pep 8

--- funcs.py
+++ funcs.py
@@ -3,9 +3,9 @@ import random
 import string
 import hashlib
 
-### Unit 2 functions ###
+# Unit 2 functions
 
-### Validation ###
+# Validation
 
 months = ['January',
           'February',
@@ -19,13 +19,15 @@ months = ['January',
           'October',
           'November',
           'December']
-          
+
+
 def valid_month(month):
     for entry in months:
         if month.lower()[:3] == entry.lower()[:3]:
             return entry
     else:
         return None
+
 
 def valid_day(day):
     if day and day.isdigit():
@@ -34,19 +36,21 @@ def valid_day(day):
         else:
             return None
 
+
 def valid_year(year):
     if year and year.isdigit():
         year = int(year)
         if year in range(1900, 2021):
             return year
 
-### html escaping ###
-#using jinja's auto escape instead.
+# html escaping
+# using jinja's auto escape instead.
 
-# symbols = {'>':'&gt;', '<':'&lt;', '"':'&quot;', '&':'&amp;'}
+symbols = {'>': '&gt;', '<': '&lt;', '"': '&quot;', '&': '&amp;'}
 
-# def escape_html(s):
-#     return s.replace('&', '&amp;').replace('>', '&gt;').replace('<', '&lt;').replace('"', '&quot;')
+
+def escape_html(s):
+    return s.replace('&', '&amp;').replace('>', '&gt;').replace('<', '&lt;').replace('"', '&quot;')
 
 
 ### ROT13 ###
@@ -78,6 +82,7 @@ alphabet = {'a': 'n', 'A': 'N',
             'y': 'l', 'Y': 'L',
             'z': 'm', 'Z': 'M'}
 
+
 def rot13(some_string):
     result = ''
     for char in some_string:
@@ -88,31 +93,35 @@ def rot13(some_string):
     return result
 
 
-### Username, Password, and Email checks ###
+# Username, Password, and Email checks
 
 def valid_username(username):
     valid = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
     return valid.match(username)
 
+
 def valid_password(password):
     valid = re.compile(r"^.{3,20}$")
     return valid.match(password)
+
 
 def valid_email(email):
     valid = re.compile(r"^[\S]+@[\S]+\.[\S]+$")
     return not email or valid.match(email)
 
 
-### Hash cookies for password and username ###
+# Hash cookies for password and username
 
 def make_salt():
     return ''.join(random.choice(string.letters) for x in xrange(5))
+
 
 def make_pw_hash(name, pw, salt = None):
     if not salt:
         salt = make_salt()
     hashbrowns = hashlib.sha256(name + pw + salt).hexdigest()
     return [hashbrowns, salt]
+
 
 def valid_pw(name, pw, h):
     salt = h[1]
